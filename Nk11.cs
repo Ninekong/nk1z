e2ad81025afe336e960c9107759b9b353917b686
Ninekong <122553014+Ninekong@users.noreply.github.com>
2023-03-13 17:06:34 +0700

    Add files via upload

---
+++ Nk11.cs
@@ -0,0 +1,16 @@
+﻿class progarm {
+    static void Main(string[] args) {
+        Console.Write("password :");
+        int w = int.Parse(Console.ReadLine());
+
+        string w = (Console.ReadLine());
+
+        
+            int md = 1 /  
+            int tu = 2 / 
+            int wd = 3 / 
+            int wt = 4 / 
+            int fr = 5 / 
+            int st = 6 /
+            int su = 7 /
+    
